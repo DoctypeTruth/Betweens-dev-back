76843a2c6b57fa3e04f4d29397b6f71175c57939
SamBess34 <besson.samuel@gmail.com>
2023-05-19 13:35:27 +0200

    REFACTO : cleaning code

--- app/controllers/messageController.js
+++ app/controllers/messageController.js
@@ -3,24 +3,21 @@ const Match = require('../models/match.js');
 const Message = require('../models/message');
 
 const messageController = {
-    
+
     // GET last messages
-    getLastMessage : async (req, res) => {
+    getLastMessage: async (req, res) => {
 
-        const matchId  = req.params.matchId;
-
+        const matchId = req.params.matchId;
         try {
-            const messages = await Message.find({ matchId }).sort({ date : -1 }).limit(1);
+            const messages = await Message.find({ matchId }).sort({ date: -1 }).limit(1);
             if (messages.length === 0) {
                 return res.status(404).json({ error: 'Message not found' });
-              }
-
+            }
             res.status(200).json(messages);
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: 'Failed to get messages' });
         }
-
     }
 }
 
